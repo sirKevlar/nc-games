0fbd908f805328da99cb6012fe641b8b38ddbfa9
Kev <kpmorel@gmail.com>
2021-07-13 13:02:21 +0100

    reviews bug fixed, review data noe fetched, CSS tweaks to fit revies page

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -4,10 +4,29 @@ import { getReviews } from "../utils/api";
 
 const Reviews = ({ reviews, setReviews }) => {
   useEffect(() => {
-    getReviews();
-  }, []);
-
-  return <div className="full-screen">REVIEWS</div>;
+    getReviews().then((reviewsFromApi) => {
+      const { reviews } = reviewsFromApi;
+      setReviews(reviews);
+    });
+  }, [setReviews]);
+  return (
+    <div className="full-screen-scroll">
+      <ul className="reviews-list">
+        {reviews.map((review) => {
+          console.log(review);
+          return (
+            <li key={review.review_id} className="review-card">
+              <img
+                src={review.review_img_url}
+                alt={review.title + "'s picture"}
+                className="review-img"
+              ></img>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Reviews;
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const gamesApi = axios.create({
-  baseURL: "https://kpm-board-games.heroku.com/api",
+  baseURL: "https://kpm-board-games.herokuapp.com/api",
 });
 
 export const getReviews = async () => {
-  const reviewObj = await gamesApi.get("/reviews");
-  console.log("called SADFASDFASDF");
-  console.log(reviewObj);
+  const reviewObj = await gamesApi.get(`/reviews`);
+  return reviewObj.data;
 };

15cc731d165ef04bba23e566f5c49a2bca262f92
Kev <kpmorel@gmail.com>
2021-07-13 21:59:48 +0100

    Dropdown menu on intro page not linking to user profile context yet

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { getUsers } from "../utils/api";
+import { selectProfileUser } from "../utils/utils";
 import gamesLogo from "../assets/logos/gamesLogoSimple.png";
 
 const Intro = ({ users, setUsers }) => {
@@ -15,14 +16,23 @@ const Intro = ({ users, setUsers }) => {
       <img src={gamesLogo} alt="games logo" />
       <div className="user-select">
         <h3>Select your user...</h3>
-        <div className="dropdown">
-          <select name="user-id">
+        <form className="dropdown">
+          <select
+            id="user-id"
+            onChange={(event) => {
+              selectProfileUser(event);
+            }}
+          >
             {users.map((user) => {
-              return <option value={user.username}>{user.username}</option>;
+              return (
+                <option key={user.username} value={user.username}>
+                  {user.username}
+                </option>
+              );
             })}
           </select>
           <button className="button">LET ME IN</button>
-        </div>
+        </form>
       </div>
     </div>
   );
